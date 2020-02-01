55cd0ad032f90f891de218a9a38df6d05992ada1
Burak Seyhan <seyhanburak9@gmail.com>
2020-02-01 11:36:01 +1100

    socil media, scroll to top and navigation bug fix

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+// navigation item click to collapse
+$(".navigation__link").on("click", function() {
+  $("#navi-toggle").click();
+});
+
+var btn = $("#back-to-top-button");
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function(e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
